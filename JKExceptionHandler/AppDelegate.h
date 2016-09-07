45216545ae21cf63ae32d1d676d458b707d72bb6
张睿 <redant_macmini_1@icloud.com>
2016-09-07 11:04:05 +0800

    Initial Commit

---
+++ JKExceptionHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKExceptionHandler
+//
+//  Created by Jack on 16/9/7.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKExceptionHandler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKExceptionHandler
+//
+//  Created by Jack on 16/9/7.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
